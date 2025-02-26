5b0afd07c03395e67b82777f56b1bb0bcba0e174
poewt <79345528+poewt@users.noreply.github.com>
2025-02-26 17:30:40 -0500

    Fix small typos

--- alu/alu_tb.v
+++ alu/alu_tb.v
@@ -20,19 +20,19 @@ module alu_tb ();
     // Task for test assertion
     task assert_test(
         input [127:0] label,
-        input signed [31:0] Expected
+        input signed [31:0] expected
     );
         begin
             $display("==================================================");
             $display("%0s", label);
             $display("A: %d", A);
             $display("B: %d", B);
-            $display("Expected: %d", Expected);
+            $display("Expected: %d", expected);
             $display("Result:   %d", Result);
             $display("Flags: V(%b), N(%b), Zero(%b)", V, N, Zero);
             
-            if (Result != Expected) begin
-                $display("❌ Test Failed! Expected: %0d, Got: %0d", Expected, Result);
+            if (Result != expected) begin
+                $display("❌ Test Failed! Expected: %0d, Got: %0d", expected, Result);
             end else if (A - B < 0 && N != 1) begin
                 $display("❌ Test Failed! Expected N: 1, Got N: %0d", N);
             end else if (A - B == 0 && Zero != 1) begin
@@ -47,7 +47,7 @@ module alu_tb ();
         $dumpfile("alu_tb.vcd");
         $dumpvars(0, alu_tb);
 
-        // ADD A > B
+        // ADD +A +B
         ALUControl = 3'b000;
         A = 32'd124;
         B = 32'd73;
